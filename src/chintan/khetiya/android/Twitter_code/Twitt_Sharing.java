f98ca10cb378d1d88dfee8890d809f65d0fb676c
Chintan Khetiya <khetiya.chintan@gmail.com>
2014-08-08 14:54:28 +0530

    Update Twitt_Sharing.java

--- src/chintan/khetiya/android/Twitter_code/Twitt_Sharing.java
+++ src/chintan/khetiya/android/Twitter_code/Twitt_Sharing.java
@@ -14,131 +14,156 @@ import chintan.khetiya.android.Twitter_code.Twitter_Handler.TwDialogListener;
 
 public class Twitt_Sharing {
 
-    private final Twitter_Handler mTwitter;
-    private final Activity activity;
-    private String twitt_msg;
-    private File image_path;
+	private final Twitter_Handler mTwitter;
+	private final Activity activity;
+	private String twitt_msg;
+	private File image_path;
 
-    public Twitt_Sharing(Activity act, String consumer_key,
-	    String consumer_secret) {
-	this.activity = act;
-	mTwitter = new Twitter_Handler(activity, consumer_key, consumer_secret);
-    }
-
-    public void shareToTwitter(String msg, File Image_url) {
-	this.twitt_msg = msg;
-	this.image_path = Image_url;
-	mTwitter.setListener(mTwLoginDialogListener);
-
-	if (mTwitter.hasAccessToken()) {
-	    // this will post data in asyn background thread
-	    showTwittDialog();
-	} else {
-	    mTwitter.authorize();
-	}
-    }
-
-    private void showTwittDialog() {
-
-	new PostTwittTask().execute(twitt_msg);
-
-    }
-
-    private final TwDialogListener mTwLoginDialogListener = new TwDialogListener() {
-
-	@Override
-	public void onError(String value) {
-	    showToast("Login Failed");
-	    mTwitter.resetAccessToken();
+	public Twitt_Sharing( Activity act, String consumer_key, String consumer_secret ) {
+		this.activity = act;
+		mTwitter = new Twitter_Handler(activity, consumer_key, consumer_secret);
 	}
 
-	@Override
-	public void onComplete(String value) {
-	    showTwittDialog();
-	}
-    };
+	public void shareToTwitter(String msg, File Image_url) {
+		this.twitt_msg = msg;
+		this.image_path = Image_url;
+		mTwitter.setListener(mTwLoginDialogListener);
 
-    void showToast(final String msg) {
-	activity.runOnUiThread(new Runnable() {
-
-	    @Override
-	    public void run() {
-		Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
-
-	    }
-	});
-
-    }
-
-    class PostTwittTask extends AsyncTask<String, Void, String> {
-	ProgressDialog pDialog;
-
-	@Override
-	protected void onPreExecute() {
-	    pDialog = new ProgressDialog(activity);
-	    pDialog.setMessage("Posting Twitt...");
-	    pDialog.setCancelable(false);
-	    pDialog.show();
-	    super.onPreExecute();
-	}
-
-	@Override
-	protected String doInBackground(String... twitt) {
-	    try {
-		// mTwitter.updateStatus(twitt[0]);
-		// File imgFile = new File("/sdcard/bluetooth/Baby.jpg");
-
-		Share_Pic_Text_Titter(image_path, twitt_msg,
-			mTwitter.twitterObj);
-		return "success";
-
-	    } catch (Exception e) {
-		if (e.getMessage().toString().contains("duplicate")) {
-		    return "Posting Failed because of Duplicate message...";
+		if (mTwitter.hasAccessToken()) {
+			// this will post data in asyn background thread
+			showTwittDialog();
+		} else {
+			mTwitter.authorize();
 		}
-		e.printStackTrace();
-		return "Posting Failed!!!";
-	    }
+	}
+
+	private void showTwittDialog() {
+
+		new PostTwittTask().execute(twitt_msg);
 
 	}
 
-	@Override
-	protected void onPostExecute(String result) {
-	    pDialog.dismiss();
+	private final TwDialogListener mTwLoginDialogListener = new TwDialogListener() {
 
-	    if (null != result && result.equals("success")) {
-		showToast("Posted Successfully");
+		@Override
+		public void onError(String value) {
+			activity.runOnUiThread(new Runnable() {
 
-	    } else {
-		showToast(result);
-	    }
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					showToast("Login Failed");
+					mTwitter.resetAccessToken();
+				}
+			});
+		}
 
-	    super.onPostExecute(result);
+		@Override
+		public void onComplete(String value) {
+			showTwittDialog();
+		}
+	};
+
+	void showToast(final String msg) {
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
+			}
+		});
+
 	}
-    }
 
-    public void Share_Pic_Text_Titter(File image_path, String message,
-	    Twitter twitter) throws Exception {
-	try {
-	    StatusUpdate st = new StatusUpdate(message);
+	class PostTwittTask extends AsyncTask<String, Void, String> {
+		ProgressDialog pDialog;
 
-	    st.setMedia(image_path);
-	    twitter.updateStatus(st);
+		@Override
+		protected void onPreExecute() {
+			pDialog = new ProgressDialog(activity);
+			pDialog.setMessage("Posting Twitt...");
+			pDialog.setCancelable(false);
+			pDialog.show();
+			super.onPreExecute();
+		}
 
-	    /*
-	     * Toast.makeText(activity, "Successfully update on Twitter...!",
-	     * Toast.LENGTH_SHORT).show();
-	     */
-	} catch (TwitterException e) {
-	    Log.d("TAG", "Pic Upload error" + e.getErrorMessage());
-	    Toast.makeText(activity,
-		    "Ooopss..!!! Failed to update on Twitter.",
-		    Toast.LENGTH_SHORT).show();
-	    throw e;
+		@Override
+		protected String doInBackground(String... twitt) {
+			try {
+				// mTwitter.updateStatus(twitt[0]);
+				// File imgFile = new File("/sdcard/bluetooth/Baby.jpg");
+
+				return Share_Pic_Text_Titter(image_path, twitt_msg, mTwitter.twitterObj);
+
+			} catch (Exception e) {
+				if (e.getMessage().toString().contains("duplicate")) {
+					return "Posting Failed because of Duplicate message...";
+				}
+				e.printStackTrace();
+				return "Posting Failed!!!";
+			}
+
+		}
+
+		@Override
+		protected void onPostExecute(final String result) {
+
+			super.onPostExecute(result);
+			pDialog.dismiss();
+			if (null != result && result.equals("success")) {
+				activity.runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						showToast(result);
+					}
+				});
+
+			}
+		}
 	}
-    }
 
-    public void Authorize_UserDetail() {
+	public String Share_Pic_Text_Titter(File image_path, String message, Twitter twitter)
+			throws Exception {
+		String Status = "";
+		try {
 
-    }
+			StatusUpdate st = new StatusUpdate(message);
+			st.setMedia(image_path);
+			twitter.updateStatus(st);
+			activity.runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+
+					Toast.makeText(activity, "Successfully update on Twitter...!",
+							Toast.LENGTH_SHORT).show();
+				}
+			});
+			Status = "success";
+			/*
+			 * Toast.makeText(activity, "Successfully update on Twitter...!",
+			 * Toast.LENGTH_SHORT).show();
+			 */
+		} catch (TwitterException e) {
+			Status = "fail";
+			Log.v("log_tag", "Pic Upload error" + e);
+			activity.runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					Toast.makeText(activity, "Ooopss..!!! Failed to update on Twitter.",
+							Toast.LENGTH_SHORT).show();
+				}
+			});
+
+		}
+		return Status;
+	}
+
+	public void Authorize_UserDetail() {
+
+	}
 }
